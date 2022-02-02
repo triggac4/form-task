443edcb901b2a8515e88012fbfb2e9b356f26594
kene3055 <koffokansi@gmail.com>
2022-02-02 13:25:28 +0100

    redux done and templateClass and filter method

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./style/App.scss";
 import FilterSection from "./layout/filter_section";
 import TemplateSection from "./layout/template-section";
-
+import TemplatesClass from "./utils/templates";
 function App() {
+    async function dor() {
+        let result = await TemplatesClass.demoTemplates();
+        console.log(result.length);
+        let result2 = TemplatesClass.filterTemplates(result, {
+            name: "ab",
+        });
+        console.log(result2.length);
+    }
+    dor();
     return (
         <div className="body">
             <div className="layout">
--- src/utils/templates.js
+++ src/utils/templates.js
@@ -7,6 +7,31 @@ class TemplatesClass {
         this.link = link;
     }
 
+    static filterTemplates(templates, { created, category, name }) {
+        const filtered = templates.filter((tempt) => {
+            let checkName = true;
+            let checkCategories = true;
+            let checkCreated = true;
+            if (name) {
+                checkName = tempt.name.includes(name);
+            }
+            if (category) {
+                if (category.toUpperCase() === "ALL") {
+                    checkCategories = true;
+                } else {
+                    checkCategories = tempt.category.includes(category);
+                }
+            }
+            if (created) {
+                //todo
+                //don't know if i should put dates are before or after
+                //so i can't do much until further instruction
+            }
+
+            return checkCategories & checkName & checkCreated;
+        });
+        return filtered;
+    }
     static async demoTemplates() {
         try {
             const response = await fetch(
